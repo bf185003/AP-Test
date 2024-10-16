137ab82cdb3202c79930a07afce2680e39bb0b72
Fu <bf185003@163.com>
2024-10-16 15:11:57 +0800

    Person修改了，简化了一些函数

--- src/Person.java
+++ src/Person.java
@@ -1,63 +1,13 @@
 public class Person {
-    public static int getMaxAge_s() {
-        return MAX_AGE;
-    }
-
-    public int getMaxAge() {
-        return MAX_AGE;
-    }
-
-    public static void setMaxAge(int maxAge) {
-        MAX_AGE = maxAge;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     private String name;
     private int age;
     private int weight;
     private String gender;
-    private final int MIN_AGE = 0;
+
+    private final int MIN_AGE = 0;//注意final的属性，要么在定义的时候赋值，要么在构造函数中定义，一旦构造好了，其他函数就不能改值了。
     private static int MAX_AGE = 150;
 
-
-    public Person(String name, int age, int weight, String gender) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
-        this.gender = gender;
-    }
-
+    //constructor构造函数，和类同名，无返回值
     public Person()
     {
         name = "";
@@ -65,6 +15,40 @@ public class Person {
         weight = 0;
         gender = "";
     }
+    //
+    public Person(String name, int age, int weight, String gender) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.gender = gender;
+    }
+
+    public String toString(){
+        return "name:" + name + " age:" + age;
+    }
+
+    public static int getMaxAge_s() {return MAX_AGE;}
+
+    public int getMaxAge() {return MAX_AGE;}
+
+    public static void setMaxAge(int maxAge) {MAX_AGE = maxAge;}
+
+    public String getName() {return name;}
+
+    public int getAge() {return age;}
+
+    public int getWeight() {return weight;}
+
+    public String getGender() {return gender;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setAge(int age) {this.age = age;}
+
+    public void setWeight(int weight) {this.weight = weight;}
+
+    public void setGender(String gender) { this.gender = gender;}
+
 
     public void grow()
     {
