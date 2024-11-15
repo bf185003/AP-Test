a6f9b56376587a3fba617c094679ab684534e615
Fu <bf185003@163.com>
2024-11-15 10:48:37 +0800

    测试秒表

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+import sampleClass.StopWatch;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Unit8 unit = new Unit8();
+     //   unit.do2dArray3();
+
+        StopWatch sw = new StopWatch();
+        sw.start();
         unit.do2dArray3();
 
+        sw.stop();
+        sw.printElapsedTime();
+
+
 //          Unit5 unit2 = new Unit5();
-          //System.out.println(Unit5);
+          //System.out.println(Unit5);34
 //          Unit5 unit1 = new Unit5();
 //          System.out.println(Unit5.attrPublicStatic);
 //        Unit5 unit = new Unit5(213);//谁是Class定义 ,谁是instance？？
