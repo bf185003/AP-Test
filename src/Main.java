071d6fa72810d940c05cbec4b7c4dbdf45d2cf60
Fu <bf185003@163.com>
2024-11-22 19:35:06 +0800

    理财方法

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
-import sampleClass.StopWatch;
+import sampleClass.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Unit8 unit = new Unit8();
+        Unit6 unit = new Unit6();
      //   unit.do2dArray3();
+        double totalIncome = 0.0;
+        for (int i = 1; i < 11; i++) {
+            totalIncome += investmentIncome(500,500,0);
+            System.out.println("今年是第 "+ i + " 年，投资总收入是："+ totalIncome);
+            System.out.println();
+        }
 
-        StopWatch sw = new StopWatch();
-        sw.start();
-        unit.do2dArray3();
-
-        sw.stop();
-        sw.printElapsedTime();
+//        StopWatch sw = new StopWatch();
+//        sw.start();
+//        unit.doArray();
+//
+//        sw.stop();
+//        sw.printElapsedTime();
 
 
 //          Unit5 unit2 = new Unit5();
@@ -29,6 +35,36 @@ public class Main {
 //        int a = unit.method_public(7) + 4;//a等于多少？
 
         //Unit5 unit2;// 注意：当你声明一个对象引用但不对其进行初始化（即没有使用 new 创建实例或赋值为已有对象），它的默认值是 null。
+    }
+
+    private static double investmentIncome(int lowRisk, int middleRisk, int highRisk){
+        double riskRate = Math.random();
+        double lowRiskIncome,middleRiskIncome,highRiskIncome;
+        //低风险收入
+        if (riskRate < 0.05)
+            lowRiskIncome = lowRisk *  -0.05;
+        else
+            lowRiskIncome = lowRisk * 0.05;
+
+        //中风险收入
+        riskRate = Math.random();
+        if (riskRate < 0.2)
+            middleRiskIncome = middleRisk * -0.2;
+        else
+            middleRiskIncome = middleRisk * 0.2;
+
+        //高风险收入
+        riskRate = Math.random();
+        if (riskRate < 0.4)
+            highRiskIncome = highRisk * -0.4;
+        else
+            highRiskIncome = highRisk * 0.4;
+
+        System.out.println("您投资组合，低风险投资：" + lowRisk + "万元，中风险投资：" + middleRisk + "万元，高风险投资："+highRisk);
+        System.out.print("您今年收益是：" + (lowRiskIncome + middleRiskIncome + highRiskIncome));
+        System.out.println(" , 其中低风险投资收益："+lowRiskIncome + "万元, 中风险收益：" + middleRiskIncome + "万元，高风险投资收益：" + highRiskIncome);
+
+        return lowRiskIncome + middleRiskIncome + highRiskIncome;
     }
 
 }
